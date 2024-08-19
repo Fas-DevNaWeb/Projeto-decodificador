c8dcb9d170dfe55b2c86ec9869c622f755fb253e
Fabiano <fasnawebdev@gmail.com>
2024-08-18 22:15:55 -0300

    acentos e minusculas bloqueados

--- script.js
+++ script.js
@@ -34,16 +34,17 @@ O Trello é uma ferramenta de uso individual para você controlar o andamento da
 Bom projeto!
 */
 /**
- * 
+ *
  * Digite seu texto aqui, estamos muito felizes por você participar desse projeto testando e dando a sua opiniao muito obrigado...
  */
 let inputTextarea = document.getElementById("texto-incriptar");
-let btnCriptografar = document.querySelector('.botao-criptogrfar');
-let imagemPesquisa = document.querySelector('.img-pesquisa');
-let fraseTexto1= document.querySelector('.msg-nao-encontrada');
-let fraseTexto2 = document.querySelector('.msg-texto-criptografar');
-let campoTextoIncriptografado = document.querySelector('.teste');
-console.log(campoTextoIncriptografado);
+let btnCriptografar = document.querySelector(".botao-criptogrfar");
+let imagemPesquisa = document.querySelector(".img-pesquisa");
+let fraseTexto1 = document.querySelector(".msg-nao-encontrada");
+let fraseTexto2 = document.querySelector(".msg-texto-criptografar");
+let campoTextoIncriptografado = document.querySelector(".teste");
+let avisoMinusculas = document.querySelector(".aviso-minusculas");
+console.log(avisoMinusculas);
 
 const chavesDeCriptografia = {
   e: "enter",
@@ -55,7 +56,11 @@ const chavesDeCriptografia = {
 
 const incriptarTexto = () => {
   let texto = inputTextarea.value;
-  let e = "",i = "",a = "",o = "",u = "";
+  let e = "",
+    i = "",
+    a = "",
+    o = "",
+    u = "";
 
   for (let i = 0; i <= texto.length; i++) {
     e = texto.replace(/e/g, `${chavesDeCriptografia.e}`);
@@ -68,24 +73,28 @@ const incriptarTexto = () => {
 };
 
 const removeElementos = () => {
-    imagemPesquisa.src = '';
-    imagemPesquisa.alt = '';
-    fraseTexto1.remove();
-    fraseTexto2.remove();
-
-}
-
+  imagemPesquisa.src = "";
+  imagemPesquisa.alt = "";
+  fraseTexto1.remove();
+  fraseTexto2.remove();
+};
 const inserirDadosEncriptados = () => {
-    const textoEmbaralhado = document.createElement('p');
-    textoEmbaralhado.classList.add('texto-embaralhado');
-    textoEmbaralhado.innerText = incriptarTexto();
-    campoTextoIncriptografado.appendChild(textoEmbaralhado);
+  const textoEmbaralhado = document.createElement("p");
+  textoEmbaralhado.classList.add("texto-embaralhado");
+  textoEmbaralhado.innerText = incriptarTexto();
 
-    
-}
-
-btnCriptografar.addEventListener('click', () => {
+  if (
+    /[\u00C0-\u00FF]/.test(incriptarTexto()) ||
+    /[A-Z]/.test(incriptarTexto())
+  ) {
+    avisoMinusculas.style.color = "red";
+  } else {
     removeElementos();
-    inserirDadosEncriptados();
-})
+    campoTextoIncriptografado.appendChild(textoEmbaralhado);
+  }
+};
+
+btnCriptografar.addEventListener("click", () => {
+  inserirDadosEncriptados();
+});
 console.log(incriptarTexto());
